0c5042c3d1118a7c761ec0655a3abded9d96a869
TheGadji <haci.ahmadov.96@gmail.com>
2023-04-16 16:57:01 +0300

    Testing DD

---
+++ Task1/ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=(local);Initial Catalog=YourDatabaseName;Integrated Security=True"; 
+
+        // Запрос на получение сотрудника с максимальной заработной платой
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT TOP 1 * FROM EMPLOYEE ORDER BY SALARY DESC";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    Console.WriteLine("Сотрудник с максимальной заработной платой:");
+                    Console.WriteLine($"Имя: {reader["NAME"]}");                    
+                    Console.WriteLine($"Заработная плата: {reader["SALARY"]}");
+                }
+                else
+                {
+                    Console.WriteLine("Сотрудников не найдено");
+                }
+            }
+            connection.Close();
+        }
+
+        // Запрос на вычисление глубины дерева руководителей в таблице Сотрудники
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "WITH CTE AS (SELECT Id, CHIEF_ID, 1 AS DEPTH FROM EMPLOYEE WHERE CHIEF_ID IS NULL UNION ALL SELECT emp.Id, emp.CHIEF_ID, c.DEPTH + 1 FROM EMPLOYEE emp INNER JOIN CTE c ON emp.CHIEF_ID = c.Id) SELECT MAX(DEPTH) AS MAX_DEPTH FROM CTE";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                object result = command.ExecuteScalar();
+                if (result != null && result != DBNull.Value)
+                {
+                    Console.WriteLine($"Максимальная длина цепочки руководителей: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Данные не найдены");
+                }
+            }
+            connection.Close();
+        }
+
+        // Запрос на получение отдела с максимальной суммарной зарплатой сотрудников
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT TOP 1 dep.* FROM DEPARTMENT dep INNER JOIN EMPLOYEE emp ON dep.Id = emp.DEPARTMENT_ID GROUP BY dep.Id, dep.NAME ORDER BY SUM(emp.SALARY) DESC";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    Console.WriteLine("Отдел с максимальной суммарной зарплатой сотрудников:");
+                    Console.WriteLine($"ID: {reader["ID"]}");
+                    Console.WriteLine($"Наименование: {reader["NAME"]}");
+                }
+                else
+                {
+                    Console.WriteLine("Отделы не найдены");
+                }
+            }
+            connection.Close();
+        }
+        // Запрос на получение сотрудника, имя которого начинается на "Р" и заканчивается на "н"
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT * FROM EMPLOYEE WHERE NAME LIKE 'Р%н'";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    Console.WriteLine("Сотрудник, имя которого начинается на 'Р' и заканчивается на 'н': ");
+                    Console.WriteLine($"Имя: {reader["NAME"]}");                    
+                }
+                else
+                {
+                    Console.WriteLine("Сотрудник не найден");
+                }
+            }
+            connection.Close();
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ Task2/ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к входному текстовому файлу:");  //C:\Война_и_мир.txt
+        string inputFile = Console.ReadLine();
+        Console.WriteLine("Введите путь к выходному текстовому файлу:"); //C:\Результат.txt
+        string outputFile = Console.ReadLine();
+
+        try
+        {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            
+            using (StreamReader reader = new StreamReader(inputFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string word in words)
+                    {
+                        if (wordCount.ContainsKey(word))
+                        {
+                            wordCount[word]++;
+                        }
+                        else
+                        {
+                            wordCount[word] = 1;
+                        }
+                    }
+                }
+            }
+
+            // Сортировка слов по убыванию
+            var sortedWords = wordCount.OrderByDescending(pair => pair.Value);
+
+            // Запись отсортированных слов и их количества в выходной файл
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                foreach (var word in sortedWords)
+                {
+                    writer.WriteLine($"{word.Key}\t\t{word.Value}");
+                }
+            }
+
+            Console.WriteLine("Результат записан в выходной файл !");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+
+        Console.ReadLine();
+    }
+}
